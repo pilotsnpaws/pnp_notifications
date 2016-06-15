2a20d773e9c8bb311a62db234adbaebf7d9b18b5
Mike Green <nekbet@gmail.com>
2016-06-15 12:30:21 -0600

    adding sendgrid api key setting

--- settings.php
+++ settings.php
@@ -3,6 +3,7 @@
 $environment = 'dev' ;
 $configPath  = '/users/michaelgreen/projects/pnp_forum/config.php' ;
 $logPath	= '/var/log/apache2/events.log' ; 
+$sgApiKey = 'sendgrid-api-key-here';
 
 
 ?>
