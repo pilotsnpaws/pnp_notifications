086ba4720ad50a37d42fbda0b423a6369c7f5104
Mike Green <nekbet@gmail.com>
2016-06-30 09:40:24 -0600

    adding new settings

--- settings.php
+++ settings.php
@@ -4,6 +4,8 @@ $environment = 'dev' ;
 $configPath  = 'ca_dev/local_config.php' ;
 $logPath	= '/dev/null' ; 
 $sgApiKey = 'x';
-
+$sendMailFlag = true; // send mail for real?
+$notificationEmailSendGridCategory = 'local test'; // category for sendgrid tracking
+$sendHoursBack = '60'; // this is how many hours back the send notification will pull topics. it is a safe guard in case the logsend fails
 
 ?>
