545a7cc6f90265f60f92766458d6e3bbb6621951
nekbet <nekbet@gmail.com>
2016-07-21 16:43:49 -0400

    add SSL

--- xport_topics.php
+++ xport_topics.php
@@ -27,26 +27,56 @@ $tableNotif = 'pnp_trip_notif_status' ; // table that knows if we sent a notif t
 $forum_id = '5' ; // we only care about the trip request forum
 
 // define forum mysqli connection
+// TODO Move this to SSL 
 $f_mysqli = new mysqli($f_server, $f_username, $f_password, $f_database);
-
-echo nl2br ("Forum database: $f_server/$f_database \n" ) ; 
-
  // Check forum connection
 if (mysqli_connect_errno($f_mysqli))
   {
-  echo "Failed to connect to forum MySQL: " . mysqli_connect_error();
-  } else { } ;
+		echo logEvent("Failed to connect to forum MySQL $f_server/$f_database: " . mysqli_connect_error());
+		exit();
+  } else {
+		echo nl2br ("Connected to forum database: $f_server/$f_database \n" ) ; 
+	} ;
 
-// define AWS mysqli connection
-$aws_mysqli = new mysqli($aws_server, $aws_username, $aws_password, $aws_database);
+// AWS connection - diff from forum as we want to SSL this traffic
+$aws_mysqli = mysqli_init();
+if (!$aws_mysqli) {
+    exit('mysqli_init failed');
+}
 
-echo nl2br ("AWS database: $aws_server/$aws_database \n\n" ) ; 
+//if (!$aws_mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+//    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+//}
 
- // Check AWS connection
-if (mysqli_connect_errno($aws_mysqli))
-  {
-  echo "Failed to connect to AWS MySQL: " . mysqli_connect_error();
-  } else { } ;
+// set SSL using AWS CA
+$aws_mysqli->ssl_set(null,null,'rds-combined-ca-bundle.pem',null,null);
+
+if (!$aws_mysqli->real_connect($aws_server, $aws_username, $aws_password, $aws_database)) {
+  echo logEvent("Failed to connect to AWS MySQL $aws_server/$aws_database: " . mysqli_connect_error());
+	exit("Failed to connect to AWS MySQL $aws_server/$aws_database: " . mysqli_connect_error());
+} else {
+		echo nl2br ("Connected to AWS database: $f_server/$f_database \n" ) ; 
+}
+
+// printf ("System status: %s\n", $aws_mysqli->stat());
+
+$res = $aws_mysqli->query("SHOW STATUS LIKE 'Ssl_cipher';");
+while($row = $res->fetch_array()) {
+	$sslCipher = $row['Value'];
+	$sslExpected = 'AES256-SHA';
+	if($sslCipher != $sslExpected) { 
+		echo logEvent("Error. SSL cipher incorrect or missing, expected $sslExpected, got: $sslCipher. Exiting."); 
+		exit();
+	} else {
+		echo("Using SSL: $sslCipher");
+		newLine();
+	}
+	
+}
+
+// end new AWS conn
+
+
 
 $maxAWSTopicId = getMaxAWS();
 $nextForumTopicId = getNextForum($maxAWSTopicId);
